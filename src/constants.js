ec3433bb1b6b5abe1fb1357f77855a9de77604da
cloud-Q <www.cloud346@gmail.com>
2025-02-14 09:31:04 +0530

    checking

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = 'youtubeclonedata'
+export const DB_NAME = 'youtubeclonedata' 
--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () =>{
     try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    // const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(`mongodb+srv://Parth:ParthYtCluster@clusteryt.rujvx.mongodb.net/?retryWrites=true&w=majority&appName=ClusterYT`);
     console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);// research about connection instance
     
     } catch (error) {
